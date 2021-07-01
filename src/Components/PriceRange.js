1fe28f5bb3fa630199dcff448c5cc8fe0e1d2893
paulBloomreach <paul.edwards@bloomreach.com>
2021-07-01 13:24:56 +0100

    price range fix

--- src/Components/PriceRange.js
+++ src/Components/PriceRange.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React, {useContext, useEffect, useReducer} from 'react';
 import {Card} from 'react-bootstrap';
 import Slider from 'rc-slider';
 import {SearchResultsContext} from '../Services/SearchResultsProvider';
@@ -8,108 +8,75 @@ const {createSliderWithTooltip} = Slider;
 const Range = createSliderWithTooltip(Slider.Range);
 
 const PriceRange = (props) => {
-  // get settings from last search
   const {
-    priceSelectedMin,
-    priceSelectedMax,
     facets,
-    submittedSearchText,
     setSearchInfo
   } = useContext(SearchTextContext);
 
   // get overall min / max values from search results
-  const {priceMin, priceMax} = useContext(SearchResultsContext);
+  const {query, priceMin, priceMax} = useContext(SearchResultsContext);
 
   // temporary storage
-  const {min: defaultMin, max: defaultMax, priceFacetSearch} = props;
+  const {min: defaultMin, max: defaultMax} = props;
 
   // rounding down the min
   let min = Math.floor(priceMin ?? defaultMin);
-
   // rounding up the max
   let max = Math.ceil(priceMax ?? defaultMax);
 
-  const [priceRangeValue, setPriceRangeValue] = useState([min, max]);
+  const [priceRangeState, setPriceRangeState] = useReducer((state, newState) => {
+    return {...state, ...newState};
+  }, {
+    range: [min, max],
+    value: [min, max],
+    text: query
+  });
 
   useEffect(() => {
-    let min = defaultMin;
-    let max = defaultMax;
+    let min = priceMin ? Math.floor(priceMin) : defaultMin;
+    let max = priceMax ? Math.ceil(priceMax) : defaultMax;
 
-    if (!priceFacetSearch) {
-      if (priceMin !== undefined) min = Math.floor(priceMin);
-      if (priceMax !== undefined) max = Math.ceil(priceMax);
-    } else {
-      if (priceSelectedMin !== undefined) min = Math.floor(priceSelectedMin);
-      if (priceSelectedMax !== undefined) max = Math.ceil(priceSelectedMax);
+    if (priceRangeState.text !== query) {
+      setPriceRangeState({range: [min, max], value: [min, max], text: query});
     }
-    if (priceRangeValue[0] < min || priceRangeValue[1] > max) {
-      setPriceRangeValue([min, max]);
-    }
-  }, [submittedSearchText, priceMin, priceMax, priceSelectedMin, priceSelectedMax]);// eslint-disable-line react-hooks/exhaustive-deps
+  }, [query, priceMin, priceMax]);// eslint-disable-line react-hooks/exhaustive-deps
 
-  // setup the facets and execute the new query
-  const handleRangeChange = (evt) => {
-    if (evt !== undefined) {
-      let rangeValue = '[' + evt[0] + ' TO ' + evt[1] + ']';
-
-      let facetsToUse = facets;
-
-      // this means the user changed the slider somehow
-      if (evt[0] !== min || evt[1] !== max) {
-        // need to REPLACE any existing sale_price facet with this new one
-        let priceRangeField = process.env.REACT_APP_PRICE_RANGE_FIELD; // generally either price or sale_price
-        const clearedFacets = facets.filter((f) => f.facetId !== priceRangeField);
-        const newFacet = {
-          facetId: priceRangeField,
-          facetName: priceRangeField,
-          facetValue: rangeValue,
-        };
-        facetsToUse = clearedFacets;
-        facetsToUse.push(newFacet);
-      }
-
-      let si = {
-        priceFacetSearch: true,
-        facets: facetsToUse,
-        start: 0,
-        priceSelectedMin: evt[0],
-        priceSelectedMax: evt[1],
-        pricePreviousMin: min,
-        pricePreviousMax: max,
-      };
-
-      setSearchInfo({...si, executeSearch: true});
-    }
-  };
-
-  // This is triggered when the user changes the slider
-  // allow a slight delay before setting values and triggering a new search
-  useEffect(() => {
-    // do nothing if the user hasn't changed anything
-    if (priceRangeValue === undefined || (priceRangeValue[0] === min && priceRangeValue[1] === max)) {
-      return;
-    }
-
-    const timeOutId = setTimeout(() => handleRangeChange(priceRangeValue), 900);
-    return () => clearTimeout(timeOutId);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [priceRangeValue, min, max]); // Ignoring not including handleRangeChange - couldn't find another way
+  const {range, value, text} = priceRangeState;
 
   return (
     <Card className={'mb-2'}>
       <Card.Header className={'text-uppercase'}>Price</Card.Header>
       <Card.Body>
         <Range
-          defaultValue={[min, max]}
-          min={min}
-          max={max}
-          value={priceRangeValue}
-          onChange={(evt) => setPriceRangeValue(evt)}
+          min={range[0]}
+          max={range[1]}
+          value={value}
+          onAfterChange={() => {
+            if (text === query) {
+              const priceRangeField = process.env.REACT_APP_PRICE_RANGE_FIELD; // generally either price or sale_price
+              const facetsToUse = facets.filter((f) => f.facetId !== priceRangeField);
+              facetsToUse.push({
+                facetId: priceRangeField,
+                facetName: priceRangeField,
+                facetValue: '[' + value[0] + ' TO ' + value[1] + ']',
+              });
+
+              const si = {
+                priceFacetSearch: true,
+                facets: facetsToUse,
+                start: 0,
+              };
+              setSearchInfo({...si, executeSearch: true});
+            }
+          }}
+          onChange={(evt) => {
+            setPriceRangeState({value: evt});
+          }}
           tipFormatter={value => `$${value}`}
-          trackStyle={[{ backgroundColor: 'red' }]}
+          trackStyle={[{backgroundColor: 'red'}]}
         />
         <div className={'w-100'}>
-          <span className={'float-left'}>${min}</span><span className={'float-right'}>${max}</span>
+          <span className={'float-left'}>${range[0]}</span><span className={'float-right'}>${range[1]}</span>
         </div>
       </Card.Body>
     </Card>
